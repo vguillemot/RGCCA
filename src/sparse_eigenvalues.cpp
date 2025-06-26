b826d0b3a976437ed1a4f0cd59c5f60a1ca5dcdb
Vincent Le Goff <vincent.legoff@cnrgh.fr>
2025-06-26 11:39:22 +0200

    changed Rcpp::export() to Rcpp::export

--- src/sparse_eigenvalues.cpp
+++ src/sparse_eigenvalues.cpp
@@ -15,20 +15,20 @@ typedef Eigen::SimplicialLDLT<laplacian> LDLTsolver;
     //;
 //}
 
-// [[Rcpp::export()]]
+// [[Rcpp::export]]
 Rcpp::XPtr<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>> LDLTsolver_new() {
     Rcpp::XPtr<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>>
         ptr (new Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>(), true);
     return ptr;
 }
 
-// [[Rcpp::export()]]
+// [[Rcpp::export]]
 void LDLTsolver_compute(Rcpp::XPtr<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>> ptr,
         const Eigen::SparseMatrix<double>& L) {
     ptr->compute(L);
 }
 
-// [[Rcpp::export()]]
+// [[Rcpp::export]]
 Eigen::VectorXd LDLTsolver_solve(Rcpp::XPtr<Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>> ptr,
         const Eigen::VectorXd& b) {
     return ptr->solve<Eigen::VectorXd>(b);
