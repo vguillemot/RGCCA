b1bfb9ba11ece9bc897b09235cd08a970bc12114
Vincent Le Goff <vincent.legoff@cnrgh.fr>
2025-07-03 15:25:59 +0200

    case for lambda_i = 0

--- R/block_update.R
+++ R/block_update.R
@@ -17,11 +17,16 @@ block_update.dual_block <- function(x, grad, mu=NULL) {
 #' @export
 block_update.graphnet_block <- function(x, grad, mu) {
   a_grad = pm(t(x$x), grad, na.rm = x$na.rm)
-    if (is.null(x$mu) || mu != x$mu) {
-      x$mu = mu
-      #LDLTsolver_compute(x$graph_laplacian$solver@pointer, 2*mu*diag(length(x$a)) + x$graph_laplacian$L)
-      compute(x$graph_laplacian, 2*x$mu*diag(length(x$a)) + 2*x$graph_laplacian$L)
-    }
+  if (x$graph_laplacian$lambda==0) {
+      x$a = 0.5*a_grad/mu + 0.5*block_project(x)$a
+      x$Y <- pm(x$x, x$a, na.rm = x$na.rm)
+      return(x)
+  }
+  if (is.null(x$mu) || mu != x$mu) {
+    x$mu = mu
+    #LDLTsolver_compute(x$graph_laplacian$solver@pointer, 2*mu*diag(length(x$a)) + x$graph_laplacian$L)
+    compute(x$graph_laplacian, 2*x$mu*diag(length(x$a)) + 2*x$graph_laplacian$L)
+  }
   #x$a <- LDLTsolver_solve(x$graph_laplacian$solver@pointer, a_grad + mu*block_project(x)$a)
   x$a <- solve(x$graph_laplacian, a_grad + x$mu*block_project(x)$a)
   x$Y <- pm(x$x, x$a, na.rm = x$na.rm)
